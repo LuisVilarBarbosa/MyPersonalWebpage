d5a4bafff74ce7946a7d7b0f208fe4f32b78acac
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2017-07-14 20:52:11 +0100

    Links and gitignore added; background and info updated
    Background changed from an external image to a color.
    Projects links added and updated.
    Page profile information updated.
    Some information related to GitHub profile removed (it is no longer
    necessary).
    .gitignore added.

--- templates/start_page.php
+++ templates/start_page.php
@@ -1,19 +1,26 @@
 ﻿<?php
 	$github = 'https://www.github.com/LuisVilarBarbosa';
+	$github_webpage = 'https://luisvilarbarbosa.github.io/';
 ?>
 
 <section id="introduction">
-	Hi! Welcome to Luís Barbosa's page.
+	Hi! Welcome to Luís Barbosa's page.<br />
+	I am a student of the Master in Informatics and Computing Engineering at Faculdade de Engenharia da Universidade do Porto.<br />
+	This webpage is hosted in a fully featured server, but the server is not always online. The link to my redirect webpage, which is hosted on GitHub, so it should be always online, is <a href=<?=$github_webpage?>><?=$github_webpage?></a>.
 </section>
 
 <section id="github">
 	My GitHub profile: <a href=<?=$github?>><?=$github?></a>
-	<p>My GitHub public profile shows little activity, because I am working in some private projects. A lot of them will become public in a few time.</p>
 </section>
 
 <section id="projects">
 	Some projects:<br />
-	<a href="LBAW">Laboratório de Bases de Dados e Aplicações Web</a>
+	<a href="CGRA/tp6/">Computer Graphics (Final project)</a><br />
+	<a href="LAIG/PW1 - Development of a Graphical Engine on WebGL/WebCGF.XMLReader/reader">Graphical Applications Laboratory (PW1 - Development of a Graphical Engine on WebGL)</a><br />
+	<a href="LAIG/PW2 - Improvement of Techniques of Use of WebGL/WebCGF.XMLReader/reader">Graphical Applications Laboratory (PW2 - Improvement of Techniques of Use of WebGL)</a><br />
+	<a href="LAIG/PW3 - 3D interface of a game/WebCGF.XMLReader/reader">Graphical Applications Laboratory (PW3 - 3D interface of a game)</a><br />
+	<a href="LBAW1656/PIU/">Database and Web Applications Laboratory (UI prototype)</a><br />
+	<a href="LBAW1656/final/">Database and Web Applications Laboratory (Final project version)</a>
 </section>
 
 <section id="interesting_links">
