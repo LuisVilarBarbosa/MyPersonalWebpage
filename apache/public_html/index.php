b6d425e798d89f7851b3232e064275af674e05ae
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2018-01-16 22:11:59 +0000

    Redirecting to HTTPS when possible (based on XAMPP)

--- apache/public_html/index.php
+++ apache/public_html/index.php
@@ -1,3 +1,11 @@
 ﻿<?php
-	header('Location: start_page.php');
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: '.$uri.'/start_page.php');
+	exit;
 ?>
+Something is wrong with the server.
