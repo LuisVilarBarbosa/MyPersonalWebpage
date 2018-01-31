7960ca49a9996b614f34f0ff15e0f964cf1710c8
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2018-01-31 23:46:06 +0000

    Links to projects and introduction updated
    .gitignore removed because it was used to ignore folders that were
    outsourced and are accessible through the new links.

--- templates/start_page.php
+++ templates/start_page.php
@@ -5,22 +5,22 @@
 
 <section id="introduction">
 	Hi! Welcome to Luís Barbosa's page.<br />
-	I am a student of the Master in Informatics and Computing Engineering at Faculdade de Engenharia da Universidade do Porto.<br />
-	This webpage is hosted in a fully featured server, but the server is not always online. The link to my redirect webpage, which is hosted on GitHub, so it should be always online, is <a href=<?=$github_webpage?>><?=$github_webpage?></a>.
+	I am a student of the Master in Informatics and Computing Engineering at Faculdade de Engenharia da Universidade do Porto.
 </section>
 
 <section id="github">
-	My GitHub profile: <a href=<?=$github?>><?=$github?></a>
+	My GitHub profile: <a href=<?=$github?>><?=$github?></a><br />
+	My GitHub webpage: <a href=<?=$github_webpage?>><?=$github_webpage?></a>
 </section>
 
 <section id="projects">
 	Some projects:<br />
-	<a href="CGRA/tp6/">Computer Graphics (Final project)</a><br />
-	<a href="LAIG/PW1 - Development of a Graphical Engine on WebGL/WebCGF.XMLReader/reader">Graphical Applications Laboratory (PW1 - Development of a Graphical Engine on WebGL)</a><br />
-	<a href="LAIG/PW2 - Improvement of Techniques of Use of WebGL/WebCGF.XMLReader/reader">Graphical Applications Laboratory (PW2 - Improvement of Techniques of Use of WebGL)</a><br />
-	<a href="LAIG/PW3 - 3D interface of a game/WebCGF.XMLReader/reader">Graphical Applications Laboratory (PW3 - 3D interface of a game)</a><br />
-	<a href="LBAW1656/PIU/">Database and Web Applications Laboratory (UI prototype)</a><br />
-	<a href="LBAW1656/final/">Database and Web Applications Laboratory (Final project version)</a>
+	<a href="https://luisbarbosaapps.herokuapp.com/feup/cgra/tp6/">Computer Graphics (Final project)</a><br />
+	<a href="https://luisbarbosaapps.herokuapp.com/feup/laig/pw1/reader/">Graphical Applications Laboratory (PW1 - Development of a Graphical Engine on WebGL)</a><br />
+	<a href="https://luisbarbosaapps.herokuapp.com/feup/laig/pw2/reader/">Graphical Applications Laboratory (PW2 - Improvement of Techniques of Use of WebGL)</a><br />
+	<a href="https://luisbarbosaapps.herokuapp.com/feup/laig/pw3/reader/">Graphical Applications Laboratory (PW3 - 3D interface of a game)</a><br />
+	<a href="https://luisbarbosaapps.herokuapp.com/feup/lbaw/PIU/">Database and Web Applications Laboratory (UI prototype)</a><br />
+	<a href="https://luisbarbosaapps.herokuapp.com/feup/lbaw/final/">Database and Web Applications Laboratory (Final project version)</a>
 </section>
 
 <section id="interesting_links">
