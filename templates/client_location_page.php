adb87fc37ece973c753db9a347cc72da8975cd30
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2018-02-01 11:55:49 +0000

    Correction to the location page so it works on Heroku
    This correction was done yesterday but was lost.

--- templates/client_location_page.php
+++ templates/client_location_page.php
@@ -3,18 +3,15 @@
 </header>
 
 <section class="client_location">
-	<?php
-		$ip = $_SERVER['REMOTE_ADDR'];
-		$details = geoip_record_by_name($ip);
-		$location = $details['city'];
-	?>
 	<p>Your IP address: <?=$ip?></p>
-	<p>Your location (city): <?=$location?></p>
+	<p>Your location (city): <?=$ipcity?></p>
 	
-	<p class="extra_data">More data: </p>
-	<?php foreach($details as $k => $v) { ?>
+	<?php if($ipdetails) { ?>
+		<p class="extra_data">More data: </p>
+		<?php foreach($ipdetails as $k => $v) { ?>
 			<p><?=$k?> => <?=$v?></p>
-	<?php } ?>
+		<?php } ?>
+	<?php } ?> 
 
 	<h5>None of this data is kept by the server. This is just indicative about the data that is possible to get from a client.</h5>
 </section>
